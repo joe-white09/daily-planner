bceb9e017059f370ec1422422bbfa35ce63ed3c0
joe-white09 <joe_white09@yahoo.com>
2022-05-19 14:27:52 -0500

    displays current day

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var currentDayEl = document.querySelector("#currentDay");
+var timeBlockEl = document.querySelector("container");
+
+var currentDate = moment().format('dddd, MMMM Do');
+
+var showCurrentDay = function() {
+    currentDayEl.innerHTML= currentDate
+};
+showCurrentDay();
+
+
+
+
+
+
+
+
+
